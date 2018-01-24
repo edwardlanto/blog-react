ee3272edf21e1d5f817ed847febc9b30d6f4fc06
Edward <you@email.com>
2018-01-24 12:46:21 -0500

    added s to http

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POST = 'fetch_post';
 export const DELETE_POST = 'delete_post';
 import axios from 'axios';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=react-blog1995';
 
 // Get Request
