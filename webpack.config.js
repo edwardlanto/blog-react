f3eea7996cbd6ca4fa0aaaa1cdb75c3ed62fcaf5
Edward <you@email.com>
2018-01-24 12:50:28 -0500

    test5

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js'],
   output: {
-    path: path.join(__dirname, '/build'),
+    path: path.join(__dirname, 'build'),
     publicPath: '/',
     filename: 'bundle.js'
   },
-
+  plugins: [new TransferWebpackPlugin([{ from: 'style' }, { from: 'assets' }])],
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
